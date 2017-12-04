adff35734e435efb2a93e27a473b79c8d7073690
Administrator <6058824@qq.com>
2017-12-04 17:12:23 +0800

    zf

--- app/src/main/java/com/shangtongyin/tools/serialport/MainActivity.java
+++ app/src/main/java/com/shangtongyin/tools/serialport/MainActivity.java
@@ -9,6 +9,7 @@ import com.zf.serial.SerialUtils;
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
     private SerialUtils serialUtils;
+    final byte[] PRINT_Reset = new byte[]{0x1b, 0x40};//重置
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         tv= (TextView) findViewById(R.id.tv_1);
         serialUtils=new SerialUtils("/dev/ttyS0",9600);
         serialUtils.open();
+        //需要回调可以开启这个功能
         serialUtils.setOnDataReceived(new SerialUtils.onDataReceived() {
             @Override
             public void Data(String data) {
@@ -23,8 +25,16 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(data);
             }
         });
+        //持续发送自己定义一个循环(打印的话就可以不需要)
         GetWeightThreadForST m=new GetWeightThreadForST();
         m.start();
+        //打印测试
+//        try {
+//            serialUtils.resetBalance(PRINT_Reset);
+//            serialUtils.resetBalance("打印测试".getBytes("GBK"));
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
     }
     private class GetWeightThreadForST extends Thread {
         public boolean isRun;
@@ -54,5 +64,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁
+        serialUtils.closeSerialPort();
     }
 }
